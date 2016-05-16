2afecdd30e5e116e09879d66ac09f4f861cbf0d5
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-16 12:09:04 -0600

    Added pdf resume.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/resume', function(req, res, next) {
-  res.render('resume');
-});
 
 module.exports = router;
